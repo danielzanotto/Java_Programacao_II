2a01bf087e200967f9d5edf86f622b093f530cf0
Daniel Zanotto <zanotto.aracena@hotmail.com>
2025-03-26 10:16:52 -0300

    Add files via upload
    Exercicio 1- Java - Utilização de classes

---
+++ Java_classe_ex1/Exercicio1_Classe/src/exercicio1_classe/Exercicio1_Classe.java
@@ -0,0 +1,13 @@
+
+package exercicio1_classe;
+
+public class Exercicio1_Classe {
+
+    public static void main(String[] args) {
+        pessoa pessoa1 = new pessoa(); //Você está instanciando na memoria ao criar o new
+        pessoa1.nome = "João";
+        pessoa1.idade = 30;
+        pessoa1.saudacao();//Invoca o metodo
+   }
+    
+}
---
+++ Java_classe_ex1/Exercicio1_Classe/src/exercicio1_classe/pessoa.java
@@ -0,0 +1,13 @@
+//
+package exercicio1_classe;
+public class pessoa {
+    //aTRIBUTOS
+    //String: Aceita todos os caracteres -> dados do tipo literal -> dado alfanumerico
+    //Public - 
+   public String nome;
+   public int idade;
+public void saudacao(){ //classe precisa ser chamada para ser executada
+    //exemplo de concatenação
+    System.out.println("Olá meu é "+ nome +"e tenho "+idade+" anos.");
+}
+}
