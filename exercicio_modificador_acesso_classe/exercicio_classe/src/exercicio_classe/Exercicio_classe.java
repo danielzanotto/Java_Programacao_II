c969fef2a3e0dc4077e428046d319c9cbc4898ff
Daniel Zanotto <zanotto.aracena@hotmail.com>
2025-04-02 11:57:29 -0300

    Add files via upload
    Primeiro exercício de modelo e classe, compartilhado

---
+++ exercicio_modificador_acesso_classe/exercicio_classe/src/exercicio_classe/Exercicio_classe.java
@@ -0,0 +1,13 @@
+
+package exercicio_classe;
+
+public class Exercicio_classe {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Pessoa pessoa1 = new Pessoa();
+    pessoa1.nome= "Lucas";
+    pessoa1.idade=30;
+    pessoa1.saudacao();
+    }
+ }
---
+++ exercicio_modificador_acesso_classe/exercicio_classe/src/exercicio_classe/Pessoa.java
@@ -0,0 +1,13 @@
+
+package exercicio_classe;
+
+class Pessoa {
+    public String nome;
+    public int idade;
+    
+    public void saudacao() {
+        
+        System.out.println("Olá, meu nome é " + nome +
+                "e tenho " + idade + " anos ");
+    }
+}
